53ee65c5208ce0ba2d2b42750055be53e7cf3f0b
hoohoo-b <teo.michelle.wt@gmail.com>
2020-04-30 15:17:59 +0800

    Specify node versions

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const passport = require('passport');
 const app = express();
 
 require('./routes/authRoutes')(app);
+
+// HEROKU has the ability to inject /set some environment variables
+// it is setup right before app is started.
+            // production     || dev
+const PORT = process.env.PORT || 5000 // fallback
+app.listen(PORT);
