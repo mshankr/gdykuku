71825f96d57318fc5002268d373ab8cde300545b
hoohoo-b <teo.michelle.wt@gmail.com>
2020-05-06 11:33:57 +0800

    UI fix to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -26,7 +26,7 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link to={this.props.auth ? "/surveys" : "/"} className="brand-logo"
+          <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo"
             style={{ marginLeft: "8px" }}>
             gdykuku
           </Link>
